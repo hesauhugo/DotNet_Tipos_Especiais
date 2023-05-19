dc1108c04ed759f062f788fb0114ae9ace138938
hesauhugo <hesau.hugo@hotmail.com>
2023-05-19 05:48:31 -0300

    feat: nulos

--- Anotacoes.md
+++ Anotacoes.md
@@ -4,3 +4,16 @@
     bool  = true, false
     bool? = true, false, null
 ```
+
+* Implementando
+* Usar HasValue sempre quando usar tipos nulos
+
+```csharp
+    bool? desejaReceberEmail = true;
+    if(desejaReceberEmail.HasValue && desejaReceberEmail.Value){
+        Console.WriteLine("O usuário optou por receber o e-mail");
+    }else{
+        Console.WriteLine("O usuário não respondeu ou optou por não receber o e-mail");
+    }
+```
+## Tipos de Nulo
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dotnet_Nuget_Serializar_Atributos.Models;
+using Newtonsoft.Json;
+
+var conteudoArquivo = File.ReadAllText("Arquivos/listaVendas.json");
+List<Venda> listaVendas = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
+foreach (var venda in listaVendas)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Desconto: {venda.Desconto}");
+}
