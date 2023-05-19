cc2f9a750e0973a7830d3ab9c060a5e2c826f01a
hesauhugo <hesau.hugo@hotmail.com>
2023-05-19 05:50:41 -0300

    feat: ternario

--- Program.cs
+++ Program.cs
@@ -5,5 +5,5 @@ var conteudoArquivo = File.ReadAllText("Arquivos/listaVendas.json");
 List<Venda> listaVendas = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
 foreach (var venda in listaVendas)
 {
-    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Desconto: {venda.Desconto}");
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Desconto: {(venda.Desconto.HasValue ? venda.Desconto.Value : "")}");
 }
